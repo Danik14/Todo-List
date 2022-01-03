d38278348da49c02f0f4e728d2d8256706d6f613
Danik <chapagana@gmail.com>
2022-01-03 21:21:28 +0600

    added task class and icon

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { header, main } from "./view";
 import { toggleProjects, toggleSidebar } from "./eventListeners";
 import style from "./style.css";
+import { Task } from "./Task";
 
 function App() {
   const body = document.getElementsByTagName("body")[0];
--- src/view.js
+++ src/view.js
@@ -38,8 +38,16 @@ function sidebar() {
   ul.classList.add("sidebar-list");
   const projects = document.createElement("div");
   projects.id = "projectsButton";
-  projects.innerHTML = "Projects";
-  domHelper.createListItems(["Home", projects], ul, "side-bar-item");
+  const home = document.createElement("div");
+  home.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-house-door" viewBox="0 0 16 16">
+  <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146zM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4H2.5z"/>
+</svg><span>Home</span>`;
+  projects.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-calendar2-check" viewBox="0 0 16 16">
+  <path d="M10.854 8.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 0 1 .708-.708L7.5 10.793l2.646-2.647a.5.5 0 0 1 .708 0z"/>
+  <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM2 2a1 1 0 0 0-1 1v11a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1H2z"/>
+  <path d="M2.5 4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H3a.5.5 0 0 1-.5-.5V4z"/>
+</svg><span>Projects</span>`;
+  domHelper.createListItems([home, projects], ul, "sidebar-list-item");
   sidebar.appendChild(ul);
   sidebar.appendChild(projectsCollapsible());
   return sidebar;
@@ -49,6 +57,7 @@ function main() {
   const main = domHelper.createElementWithClass("section", "main");
   main.id = "main";
   main.appendChild(sidebar());
+  main.append("123");
   return main;
 }
 
