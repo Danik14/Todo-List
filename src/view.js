4c831d0c69be274adacda520f65ba95bbab8713f
Danik <chapagana@gmail.com>
2022-01-09 12:09:49 +0600

    remade popups

--- src/view.js
+++ src/view.js
@@ -5,6 +5,9 @@ function header() {
   const logo = domHelper.createElementWithClass("div", "logo");
   logo.id = "sidebarButton";
   const addTask = domHelper.createElementWithClass("button", "btn-header");
+  addTask.setAttribute("type", "button");
+  addTask.setAttribute("data-bs-toggle", "modal");
+  addTask.setAttribute("data-bs-target", "#staticBackdrop");
   const button = domHelper.createElementWithClass("button", "btn-header");
   button.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-justify" viewBox="0 0 16 16">
   <path fill-rule="evenodd" d="M2 12.5a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z"/>
@@ -21,11 +24,21 @@ function header() {
 function projectsCollapsible() {
   const projectsCollapsible = domHelper.createElementWithClass(
     "div",
-    "projects-collapsible"
+    "projectsCollapsible"
   );
   projectsCollapsible.id = "projectsCollapsible";
   const ul = document.createElement("ul");
-  domHelper.createListItems(["Project 1", "Project 2"], ul, "projects-item");
+  const button = domHelper.createElementWithClass("button", "newProjectButton");
+  button.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="19" height="19" fill="currentColor" class="bi bi-window-plus" viewBox="0 0 16 16">
+  <path d="M2.5 5a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1ZM4 5a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1Zm2-.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0Z"/>
+  <path d="M0 4a2 2 0 0 1 2-2h11a2 2 0 0 1 2 2v4a.5.5 0 0 1-1 0V7H1v5a1 1 0 0 0 1 1h5.5a.5.5 0 0 1 0 1H2a2 2 0 0 1-2-2V4Zm1 2h13V4a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v2Z"/>
+  <path d="M16 12.5a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Zm-3.5-2a.5.5 0 0 0-.5.5v1h-1a.5.5 0 0 0 0 1h1v1a.5.5 0 0 0 1 0v-1h1a.5.5 0 0 0 0-1h-1v-1a.5.5 0 0 0-.5-.5Z"/>
+</svg> New Project`;
+  domHelper.createListItems(
+    ["Project 1", "Project 2", button],
+    ul,
+    "projects-item"
+  );
   projectsCollapsible.appendChild(ul);
 
   return projectsCollapsible;
@@ -53,11 +66,47 @@ function sidebar() {
   return sidebar;
 }
 
+function popUpNewTask() {
+  const div = document.createElement("div");
+  div.innerHTML = `
+<!-- Modal -->
+<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="staticBackdropLabel">Add new Task</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form>
+          <div class="mb-3">
+            <label for="task-name" class="col-form-label">Title:</label>
+            <input type="text" class="form-control" id="task-name">
+          </div>
+          <div class="mb-3">
+            <label for="description-text" class="col-form-label">Description:</label>
+            <textarea class="form-control" id="description-text"></textarea>
+          </div>
+          <i class="bi-alarm"></i>
+          
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Understood</button>
+      </div>
+    </div>
+  </div>
+</div>`;
+
+  return div;
+}
+
 function main() {
   const main = domHelper.createElementWithClass("section", "main");
   main.id = "main";
   main.appendChild(sidebar());
-  main.append("123");
+  main.append(popUpNewTask());
   return main;
 }
 
